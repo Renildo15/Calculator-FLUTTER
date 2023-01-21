53dac08c7a49c6e5c77d46bd51ac7e8da4cbe5bf
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-21 00:05:23 -0300

    feat: finalizando o projeto #2

--- calculator/lib/screens/calculator.dart
+++ calculator/lib/screens/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 import '../models/memory.dart';
@@ -17,6 +18,9 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
     return MaterialApp(
       home: Column(
         children: <Widget>[
