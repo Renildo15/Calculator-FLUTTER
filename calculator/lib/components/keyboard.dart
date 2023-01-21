fd72ff06daa1965f98e66307cd0efc354f22ae10
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-20 23:32:34 -0300

    feat: finalizando display #2

--- calculator/lib/components/keyboard.dart
+++ calculator/lib/components/keyboard.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'button_row.dart';
 import 'button.dart';
 class Keyboard extends StatelessWidget{
+  final void Function(String) cb;
+
+  Keyboard(this.cb);
+
   @override
   Widget build(BuildContext context){
     return Container(
@@ -9,32 +13,36 @@ class Keyboard extends StatelessWidget{
       child: Column(
         children: <Widget>[
           ButtonRow([
-            Button(text: 'AC', big: true),
-            Button(text: '%'),
-            Button(text: '/'),
+            Button.big(text: 'AC', color:Button. DARK, cb:cb),
+            Button(text: '%', color:Button. DARK, cb:cb),
+            Button.operation(text: '/', cb:cb),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
-            Button(text: '7'),
-            Button(text: '8'),
-            Button(text: '9'),
-            Button(text: 'x'),
+            Button(text: '7', cb:cb),
+            Button(text: '8', cb:cb),
+            Button(text: '9', cb:cb),
+            Button.operation(text: 'x', cb:cb),
           ]),
+           SizedBox(height: 1),
           ButtonRow([
-            Button(text: '4'),
-            Button(text: '5'),
-            Button(text: '6'),
-            Button(text: '-'),
+            Button(text: '4', cb:cb),
+            Button(text: '5', cb:cb),
+            Button(text: '6', cb:cb),
+            Button.operation(text: '-', cb:cb),
           ]),
+           SizedBox(height: 1),
           ButtonRow([
-            Button(text: '1'),
-            Button(text: '2'),
-            Button(text: '3'),
-            Button(text: '+'),
+            Button(text: '1', cb:cb),
+            Button(text: '2', cb:cb),
+            Button(text: '3', cb:cb),
+            Button.operation(text: '+', cb:cb),
           ]),
+           SizedBox(height: 1),
           ButtonRow([
-            Button(text: '0', big: true),
-            Button(text: '.'),
-            Button(text: '='),
+            Button.big(text: '0', cb:cb),
+            Button(text: '.', cb:cb),
+            Button.operation(text: '=', cb:cb),
           ]),
         ],
       ),
--- calculator/lib/screens/calculator.dart
+++ calculator/lib/screens/calculator.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
-class Calculator extends StatelessWidget{
+import '../models/memory.dart';
+class Calculator extends StatefulWidget{
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+  _onPressed(String command){
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Column(
         children: <Widget>[
-          Display('123.45'),
-          Keyboard(),
+          Display(memory.value),
+          Keyboard(_onPressed),
         ],
       )
     );
